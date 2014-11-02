22415edcb6571620ac95e4c0933a0eaa4633d8f3
Brad <brad@Brads-MacBook-Air.local>
2014-11-02 13:03:29 +1100

    vend request path update

--- lib/ipay/IpayRequest.rb
+++ lib/ipay/IpayRequest.rb
@@ -2,27 +2,30 @@ require 'erb'
 require 'pathname'
 
 class IpayRequest < ERB
-  @@request_template_path = Pathname.new "./lib/ipay/requests/"
-  # @@request_template_path = "./ipay/requests/"
+  # @request_template_path = Pathname.new "./lib/ipay/requests/"
+  # @request_template_path = "./requests/"
+
   # def method_missing(method, *args, &block)
   #   p "no template #{method} found"
   # end
 
   def initialize(template = nil, params = {})
+    @request_template_path = File.expand_path("lib/ipay/requests/")
     @params = params
     @template = self.send(template)
     super(@template)
   end
 
   def vend_request
-    template_file = File.read("#{@@request_template_path}vend_request.erb")
+    p  "**reading: #{@request_template_path}/vend_request.erb"
+    template_file = File.read("#{@request_template_path}/vend_request.erb")
     template_file.gsub!("\n", '')
 
     return template_file
   end
 
   def vend_rev_request
-    template_file = File.read("#{@@request_template_path}vend_rev_request.erb")
+    template_file = File.read("#{@request_template_path}/vend_rev_request.erb")
     template_file.gsub!("\n", '')
 
     return template_file
